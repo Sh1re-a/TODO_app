9741b270508c3b90c3c8feb1e90791c5245405d5
kambii <kambinho123@hotmail.com>
2023-01-24 14:06:04 +0100

    updated entity class

---
+++ src/main/java/stigrupp7/todo/model/Todo.java
@@ -0,0 +1,45 @@
+package stigrupp7.todo.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String description;
+
+    public Todo(String description) {
+        this.description = description;
+    }
+
+    public Todo() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Todo{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
