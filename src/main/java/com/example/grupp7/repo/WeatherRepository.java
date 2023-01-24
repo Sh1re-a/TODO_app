970490da270058624bc022d1ac0af8332b65328c
fethi9an <martinfethi97@hotmail.com>
2023-01-24 12:54:26 +0100

    Changed to TODO

--- src/main/java/com/example/grupp7/repo/WeatherRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.grupp7.repo;
-
-import com.example.grupp7.model.Weather;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WeatherRepository extends JpaRepository<Weather, Long> {
-}
